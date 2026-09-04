6a5fccf3f6fbed7917f23d0fee75e8592c9426c2
codebygunjan <Gunjankhudaniya414@gmail.com>
2026-09-04 22:18:33 +0530

    solved bugs

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={clerkPubKey}>
-    <BrowserRouter basename="/HADAN">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </ClerkProvider>,
